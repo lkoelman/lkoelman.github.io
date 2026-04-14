9bca12d4f06e290edf17dd5a2b01f129a8db8d6b
Lucas Koelman <lucas.koelman@gmail.com>
2026-04-14 16:59:53 +0100

    Add custom gruvbox-inspired blog template

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Lucas Koelman';
+export const SITE_DESCRIPTION = 'Writing about software, systems, and side projects.';
